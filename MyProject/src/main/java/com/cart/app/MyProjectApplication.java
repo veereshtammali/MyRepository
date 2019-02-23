da07dc9289b345d49940294ca6d02f35da3bad03
Veeresham <veeresham.tammali103@gmail.com>
2019-02-23 08:49:53 +0530

    SOP added

--- MyProject/src/main/java/com/cart/app/MyProjectApplication.java
+++ MyProject/src/main/java/com/cart/app/MyProjectApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyProjectApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Init");
 		SpringApplication.run(MyProjectApplication.class, args);
 	}
 
